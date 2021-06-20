cf5681518d611679bda7560b5b40d12830ccc465
Evgeni67 <evgeni313@abv.bg>
2021-06-21 00:15:19 +0500

    done

--- shared-library/src/App.js
+++ shared-library/src/App.js
@@ -1,18 +1,22 @@
 import CalendarComponent from "./sharedComponents/calendarComponent/CalendarComponent";
 import "bootstrap/dist/css/bootstrap.css";
 import React, { Component } from "react";
-import {
-  Container
-} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 class App extends Component {
- 
   render() {
     return (
       <>
-          <Container>
-            <CalendarComponent />
-          </Container>
+        <Container>
+          <CalendarComponent
+            events={[
+              {
+                day: "Tue Jun 01 2021 00:00:00 GMT+0500 (Pakistan Standard Time)",
+                type: "green",
+              },
+            ]}
+          />
+        </Container>
       </>
     );
   }
--- shared-library/src/sharedComponents/calendarComponent/CalendarComponent.jsx
+++ shared-library/src/sharedComponents/calendarComponent/CalendarComponent.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./CalendarComponent.css";
 import { Container, Row, Col } from "react-bootstrap";
-import {ImArrowRight} from "react-icons/im"
-import {ImArrowLeft} from "react-icons/im"
+import { ImArrowRight } from "react-icons/im";
+import { ImArrowLeft } from "react-icons/im";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { AiFillMinusCircle as HiMinusCircle } from "react-icons/ai";
 class CalendarComponent extends Component {
   state = {
     greenEvents: [],
     redEvents: [],
     orangeEvents: [],
+    notes: [],
+    seeNotes: false,
     isDateSelected: false,
     dateSelected: {},
     today: new Date(),
@@ -129,46 +133,149 @@ class CalendarComponent extends Component {
         (this.state.allDaysInTheMonth.length + this.state.startArrayLenght),
     });
   };
+
+  reduceGreenEvents = async () => {
+    var greenEventsNew = this.state.greenEvents;
+    if (
+      greenEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 0
+    ) {
+    } else {
+      //custom remove at
+      for (let i = 0; i < greenEventsNew.length; i++) {
+        if (
+          new Date(greenEventsNew[i]).getTime() ===
+          this.state.dateSelected.getTime()
+        ) {
+          greenEventsNew.splice(i, 1);
+          i = greenEventsNew.length;
+        }
+      }
+      this.setState({ greenEvents: greenEventsNew });
+    }
+  };
   reduceOrangeEvents = async () => {
-    const orangeEventsNew = this.state.orangeEvents;
-    
-    if(orangeEventsNew.length === 3){
-      window.alert("Max 3 per type")
-    }else{
-      orangeEventsNew.slice(0,orangeEventsNew.length());
+    var orangeEventsNew = this.state.orangeEvents;
+    if (
+      orangeEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 0
+    ) {
+    } else {
+      //custom remove at
+      for (let i = 0; i < orangeEventsNew.length; i++) {
+        if (
+          new Date(orangeEventsNew[i]).getTime() ===
+          this.state.dateSelected.getTime()
+        ) {
+          orangeEventsNew.splice(i, 1);
+          i = orangeEventsNew.length;
+        }
+      }
       this.setState({ orangeEvents: orangeEventsNew });
     }
-   
+  };
+  reduceRedEvents = async () => {
+    var redEventsNew = this.state.redEvents;
+    if (
+      redEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 0
+    ) {
+    } else {
+      //custom remove at
+      for (let i = 0; i < redEventsNew.length; i++) {
+        if (
+          new Date(redEventsNew[i]).getTime() ===
+          this.state.dateSelected.getTime()
+        ) {
+          redEventsNew.splice(i, 1);
+          i = redEventsNew.length;
+        }
+      }
+      this.setState({ redEvents: redEventsNew });
+    }
   };
   saveInRedEvents = async () => {
     const redEventsNew = this.state.redEvents;
-    if(redEventsNew.length === 3){
-      window.alert("Max 3 per type")
-    }else{
-    redEventsNew.push(new Date(this.state.dateSelected).getTime());
-    this.setState({ redEvents: redEventsNew });
+    if (
+      redEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 3
+    ) {
+      window.alert("Max 3 per type");
+    } else {
+      redEventsNew.push(new Date(this.state.dateSelected).getTime());
+      this.setState({ redEvents: redEventsNew });
     }
   };
   saveInOrangeEvents = async () => {
     const orangeEventsNew = this.state.orangeEvents;
-    if(orangeEventsNew.length === 3){
-      window.alert("Max 3 per type")
-    }else{
+    if (
+      orangeEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 3
+    ) {
+      window.alert("Max 3 per type");
+    } else {
       orangeEventsNew.push(new Date(this.state.dateSelected).getTime());
       this.setState({ orangeEvents: orangeEventsNew });
     }
-   
   };
   saveInGreenEvents = async () => {
     const greenEventsNew = this.state.greenEvents;
-    if(greenEventsNew.length === 3){
-      window.alert("Max 3 per type")
-    }else{
-    greenEventsNew.push(new Date(this.state.dateSelected).getTime());
-    this.setState({ greenEvents: greenEventsNew })
-    };
+    if (
+      greenEventsNew.filter(
+        (date) => new Date(date).getTime() === this.state.dateSelected.getTime()
+      ).length === 3
+    ) {
+      window.alert("Max 3 per type");
+    } else {
+      greenEventsNew.push(new Date(this.state.dateSelected).getTime());
+      this.setState({ greenEvents: greenEventsNew });
+    }
   };
   componentDidMount = async () => {
+    console.log("->",this.props.events);
+    //we pass the incoming data for the events in our state so we can visualize it
+    let greenEventsFromProps1 = this.props.events.filter(
+      (event) => event.type === "green"
+    );
+    console.log(greenEventsFromProps1)
+    let greenEventsFromProps = [];
+
+    if (greenEventsFromProps1.length > 0) {
+      greenEventsFromProps1.forEach((event) =>
+        greenEventsFromProps.push(event.day)
+      );
+    }
+
+    let orangeEventsFromProps1 = this.props.events.filter(
+      (event) => event.type === "orange"
+    );
+    let orangeEventsFromProps = [];
+
+    if (orangeEventsFromProps1.length > 0) {
+      orangeEventsFromProps1.forEach((event) =>
+        orangeEventsFromProps.push(event.day)
+      );
+    }
+
+    let redEventsFromProps = [];
+    let redEventsFromProps1 = this.props.events.filter(
+      (event) => event.type === "orange"
+    );
+
+    if (redEventsFromProps1.length > 0) {
+      redEventsFromProps1.forEach((event) =>
+        redEventsFromProps.push(event.day)
+      );
+    }
+
+    this.setState({ orangeEvents: orangeEventsFromProps });
+    this.setState({ redEvents: redEventsFromProps });
+    this.setState({ greenEvents: greenEventsFromProps });
     //Getting the current year and month
     var today = new Date();
     this.setState({ currentYear: today.getFullYear() });
@@ -264,50 +371,45 @@ class CalendarComponent extends Component {
   render() {
     return (
       <>
-        <Container className="calendarContainer mt-2">
+        <Container className="calendarContainer mt-2 noselect">
           <h5 className="calendarText d-flex justify-content-center">
             Calendar
           </h5>
-          <Row className="d-flex justify-content-center">
-           
-          </Row>
-          <Row className = "monthYearRow">
+          <Row className="d-flex justify-content-center"></Row>
+          <Row className="monthYearRow ">
             <Col
               sm={2}
-              className="d-flex justify-content-center"
+              className="d-flex justify-content-center "
               onClick={() => this.changeCurrentMonth(1)}
             >
               {" "}
-              <ImArrowLeft className ="arrow"/>
+              <ImArrowLeft className="arrow" />
             </Col>{" "}
-            <Col
-              sm={8}
-              className="d-flex justify-content-center"
-            >
+            <Col sm={8} className="d-flex justify-content-center ">
               {" "}
               {this.state.readyToRender ? (
-              <p className="yearMonth d-flex justify-content-center">
-                {this.state.currentYear}{" "}
-                {
-                  this.state.monthNames[
-                    new Date(this.state.allDaysInTheMonth[0].day).getMonth()
-                  ]
-                }
-              </p>
-            ) : (
-              1
-            )}
+                <p className="yearMonth d-flex justify-content-center">
+                  {this.state.currentYear}{" "}
+                  {
+                    this.state.monthNames[
+                      new Date(this.state.allDaysInTheMonth[0].day).getMonth()
+                    ]
+                  }
+                </p>
+              ) : (
+                1
+              )}
             </Col>
             <Col
               sm={2}
-              className="d-flex justify-content-center"
+              className="d-flex justify-content-center "
               onClick={() => this.changeCurrentMonth(0)}
             >
               {" "}
-              <ImArrowRight className ="arrow"/>
+              <ImArrowRight className="arrow" />
             </Col>
           </Row>
-          <Row>
+          <Row className="">
             <Col className="date">Mon</Col>
             <Col className="date">Tue</Col>
             <Col className="date">Wed</Col>
@@ -319,7 +421,7 @@ class CalendarComponent extends Component {
               <>
                 {" "}
                 {this.state.allDaysInThePrevMonth.map((day) => (
-                  <Col className="dateFromPrevMonth">
+                  <Col className="dateFromPrevMonth ">
                     {new Date(day.day).getDate()}
                   </Col>
                 ))}
@@ -328,18 +430,18 @@ class CalendarComponent extends Component {
                     <Col
                       className={
                         new Date(day.day).getDate() ===
-                          this.state.today.getDate() && new Date(day.day).getMonth() ===
-                          this.state.today.getMonth() && new Date(day.day).getYear() ===
+                          this.state.today.getDate() &&
+                        new Date(day.day).getMonth() ===
+                          this.state.today.getMonth() &&
+                        new Date(day.day).getYear() ===
                           this.state.today.getYear()
-                          
-                   
-                          ? "dateToday"
-                          : "date"
+                          ? "dateToday "
+                          : "date "
                       }
                       onClick={() => this.selectDate(day.day)}
                     >
                       <p className="dateP"> {new Date(day.day).getDate()} </p>
-                      <table className = "mt-1">
+                      <table className="mt-1">
                         <tr></tr>
                         <tr className="evenets">
                           {this.state.redEvents
@@ -349,7 +451,7 @@ class CalendarComponent extends Component {
                                 new Date(day.day).getTime()
                             )
                             .map((event) => (
-                              <td className = "eventDot">🔴</td>
+                              <td className="eventDot">🔴</td>
                             ))}
                         </tr>
                         <tr className="evenets">
@@ -360,7 +462,7 @@ class CalendarComponent extends Component {
                                 new Date(day.day).getTime()
                             )
                             .map((event) => (
-                              <td className = "eventDot">🟠</td>
+                              <td className="eventDot">🟠</td>
                             ))}
                         </tr>
                         <tr className="evenets">
@@ -371,7 +473,7 @@ class CalendarComponent extends Component {
                                 new Date(day.day).getTime()
                             )
                             .map((event) => (
-                              <td className = "eventDot">🟢</td>
+                              <td className="eventDot">🟢</td>
                             ))}
                         </tr>
                         <tr className="evenets"></tr>
@@ -382,7 +484,7 @@ class CalendarComponent extends Component {
                 {this.state.allDaysInTheMonth
                   .slice(0, this.state.endArrayLenght)
                   .map((day) => (
-                    <Col className="dateFromNextMonth">
+                    <Col className="dateFromNextMonth ">
                       {new Date(day.day).getDate()}
                     </Col>
                   ))}
@@ -395,22 +497,45 @@ class CalendarComponent extends Component {
             <>
               {" "}
               <Row className="mt-4">
-                <Col
-                  className="addDot d-flex justify-content-center"
-                  onClick={() => this.saveInRedEvents()}
-                >
-                  Red Dot 
+                <Col className="addDot d-flex justify-content-center">
+                  <HiMinusCircle
+                    className="minus"
+                    onClick={() => this.reduceRedEvents()}
+                  />{" "}
+                  <text className="redDot"> 🔴 </text>
+                  <BsFillPlusCircleFill
+                    className="plus"
+                    onClick={() => this.saveInRedEvents()}
+                  />
                 </Col>
-                <Col className="addDot d-flex justify-content-center" onClick={() => this.saveInOrangeEvents()}>
-                  Orange Dot
+                <Col className="addDot d-flex justify-content-center">
+                  <HiMinusCircle
+                    className="minus"
+                    onClick={() => this.reduceOrangeEvents()}
+                  />{" "}
+                  <text className="redDot"> 🟠 </text>
+                  <BsFillPlusCircleFill
+                    className="plus"
+                    onClick={() => this.saveInOrangeEvents()}
+                  />
                 </Col>
-                <Col className="addDot d-flex justify-content-center"onClick={() => this.saveInGreenEvents()}>
-                  Green Dot
+                <Col className="addDot d-flex justify-content-center">
+                  <HiMinusCircle
+                    className="minus"
+                    onClick={() => this.reduceGreenEvents()}
+                  />{" "}
+                  <text className="redDot"> 🟢 </text>
+                  <BsFillPlusCircleFill
+                    className="plus"
+                    onClick={() => this.saveInGreenEvents()}
+                  />
                 </Col>
               </Row>
             </>
           ) : (
-            <> </>
+            <h5 className="d-flex justify-content-center mt-5 ">
+              Select a date{" "}
+            </h5>
           )}
         </Container>
       </>
